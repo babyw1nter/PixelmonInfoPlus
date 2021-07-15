c77db80295e7c55c57dbc641b9bc279be7bf0d14
hhui64 <907322015@qq.com>
2021-07-15 17:38:31 +0800

    refactor: 重构热键绑定类

--- src/main/java/io/github/hhui64/PixelmonInfoPlus/listeners/HotKeyListener.java
+++ src/main/java/io/github/hhui64/PixelmonInfoPlus/listeners/HotKeyListener.java
@@ -1,19 +1,19 @@
-package io.github.hhui64.PixelmonInfoPlus.listeners;
-
-import io.github.hhui64.PixelmonInfoPlus.hotkey.HotKey;
-import io.github.hhui64.PixelmonInfoPlus.gui.ivev.IVEVGuiContainer;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
-import net.minecraftforge.fml.common.gameevent.InputEvent;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
-public class HotKeyListener {
-    private static final Logger logger = LogManager.getLogger("HOTKEY-EVENT");
-
-    @SubscribeEvent
-    public void onKeyInput(InputEvent.KeyInputEvent event) {
-        if (HotKey.SHOW_IVEVGUI_KEY_BINDING.isPressed()) {
-            IVEVGuiContainer.open();
-        }
-    }
-}
+//package io.github.hhui64.PixelmonInfoPlus.listeners;
+//
+//import io.github.hhui64.PixelmonInfoPlus.hotkey.HotKeyManager;
+//import io.github.hhui64.PixelmonInfoPlus.gui.ivev.IVEVGuiContainer;
+//import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+//import net.minecraftforge.fml.common.gameevent.InputEvent;
+//import org.apache.logging.log4j.LogManager;
+//import org.apache.logging.log4j.Logger;
+//
+//public class HotKeyListener {
+//    private static final Logger logger = LogManager.getLogger("HOTKEY-EVENT");
+//
+//    @SubscribeEvent
+//    public void onKeyInput(InputEvent.KeyInputEvent event) {
+//        if (HotKeyManager.SHOW_IVEVGUI_KEY_BINDING.isPressed()) {
+//            IVEVGuiContainer.open();
+//        }
+//    }
+//}
