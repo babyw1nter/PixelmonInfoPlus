9dfcd77e567728198749d9d091a6f83055fc6e9e
hhui64 <907322015@qq.com>
2021-08-01 11:18:37 +0800

    release: v1.0.1

--- src/main/java/io/github/hhui64/pixelmoninfoplus/PixelmonInfoPlus.java
+++ src/main/java/io/github/hhui64/pixelmoninfoplus/PixelmonInfoPlus.java
@@ -16,7 +16,7 @@ import org.apache.logging.log4j.LogManager;
 public class PixelmonInfoPlus {
     public static final String MODID = "pixelmoninfoplus";
     public static final String NAME = "Pixelmon Info Plus";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.0.1";
     public static final String DEPENDENCIES = "required-after:pixelmon";
 
     public static Logger logger = LogManager.getLogger("PixelmonInfoPlus");
